d1c2ee018808cd0aa0e91205bda837b0664f391e
Fadi88 <fady88@gmail.com>
2018-07-11 09:14:16 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -28,13 +28,14 @@ std::string hasData(std::string s) {
   return "";
 }
 
-int main()
+int main(int argc, char* argv[])
 {
   uWS::Hub h;
 
   PID pid;
   // TODO: Initialize the pid variable.
-
+  pid.Init(atof(argv[1]) , atof(argv[2]) , atof(argv[3]) );
+  
   h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
     // The 4 signifies a websocket message
@@ -57,7 +58,9 @@ int main()
           * NOTE: Feel free to play around with the throttle and speed. Maybe use
           * another PID controller to control the speed!
           */
-          
+          pid.UpdateError(cte);
+          steer_value = -1 * pid.TotalError();
+
           // DEBUG
           std::cout << "CTE: " << cte << " Steering Value: " << steer_value << std::endl;
 
